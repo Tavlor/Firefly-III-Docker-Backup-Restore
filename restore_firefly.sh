3813f761a7287b25c0741817a62584a6718a8470
Taylor Smith <tavlor@protonmail.com>
2021-06-13 13:25:52 -0500

    remove leftover code that caused errors

--- restore_firefly.sh
+++ restore_firefly.sh
@@ -7,25 +7,21 @@ BACKUP_DIR="$(realpath $1)"
 # generate volume names - if there is a prefix, it is the 2nd arg
 VOL_ROOT="firefly_iii"
 if [ "$#" -eq 2 ]; then
-    VOL_ROOT="${2}_${VOL_ROOT}"
+    VOL_ROOT="$2_${VOL_ROOT}"
 fi
 VOLUMES="${VOL_ROOT}_upload ${VOL_ROOT}_db"
 
 # --------------------
 
-VOL_ROOT="${VOL_PREFIX}_firefly_iii"
-VOLUMES="${VOL_ROOT}_upload ${VOL_ROOT}_db"
-
 echo "restoring from archives in $BACKUP_DIR"
 
-# check that all volumes do not exists
+# check that all volumes do not exist
 for VOL in $VOLUMES
 do
     echo "-- checking that volume $VOL doesn't exist"
     if docker volume ls -q | grep -q "^$VOL$"
     then
-        errornotification "Cannot restore - volume exists" \
-            "Volume $VOL already exists! Please rename or delete it."
+	echo "Volume $VOL already exists! Please rename or delete it."
         exit 1
     fi
 done
